6c37be106af1fcd0efd2084bc91ff36a23dfa59c
Jaco de Villiers <62937916+Jaco-Daan@users.noreply.github.com>
2025-04-21 19:12:45 +0200

    Rework `.bat` file

--- run_ui.bat
+++ run_ui.bat
@@ -1,4 +1,18 @@
 @echo off
 cd /d "%~dp0"
+
+:: Create virtual environment if it doesn't exist
+if not exist venv (
+    echo Creating virtual environment...
+    python -m venv venv
+)
+
+:: Activate it
+call venv\Scripts\activate.bat
+
+:: Install dependencies if needed
+pip install -r requirements.txt
+
+:: Run the app
 streamlit run .\interface\ui_app.py
 pause
