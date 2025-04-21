2e9d8c43cb3ec92c7ff5bac686b01d172b71c799
Jaco de Villiers <62937916+Jaco-Daan@users.noreply.github.com>
2025-04-21 17:22:52 +0200

    minor fixes

--- interface/ui_app.py
+++ interface/ui_app.py
@@ -31,12 +31,8 @@ class GenderLaw(Enum):
 
 def get_resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except AttributeError:
-        base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.abspath(os.path.join(base_path, '..', relative_path))
 
 # Load config
 def load_config(config_path):
---
+++ run_ui.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+streamlit run .\interface\ui_app.py
+pause
